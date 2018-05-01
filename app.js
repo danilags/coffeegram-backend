a4d6df777e8e10771bc29991c1ed15c19af53b55
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-05-01 14:24:08 +0700

    structuring app and mongoose connection

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./app/routes/index');
+var usersRouter = require('./app/routes/users');
 
 var app = express();
 
@@ -14,10 +17,22 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+// connection
+mongoose.Promise = global.Promise
+const db = mongoose.connection
+
+var url = 'mongodb://localhost:27017/coffeegram';
+mongoose.connect(url)
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongo is connected !')
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
